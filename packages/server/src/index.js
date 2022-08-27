8148633af5df9b34397d92826e2c7c7025574637
qxb3 <qxbthree@gmail.com>
2022-08-27 21:09:47 +0800

    Revert "server: using vercel serverless"
    This reverts commit 9250d0d0b5f4a489afb3acb70551e21e350fb5c7.

--- packages/server/src/index.js
+++ packages/server/src/index.js
@@ -3,17 +3,12 @@ require('dotenv/config')
 const { build } = require('./app')
 const { connectToDb } = require('./lib/utils/db')
 
-const app = build()
-
 connectToDb(() => {
+  const app = build()
+
   app.listen({ port: process.env.PORT || 5000 }, (err, address) => {
     if (err) return console.log(err)
 
     console.log(`Server is running at ${address}`)
   })
 })
-
-module.exports = async (req, res) => {
-  await app.ready()
-  app.server.emit('request', req, res)
-}
