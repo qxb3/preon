4d886eca32337563610b849c02ff3b179d2b8bbf
qxb3 <qxbthree@gmail.com>
2022-08-27 18:33:52 +0800

    using vercel-sapper

--- packages/client/src/server.js
+++ packages/client/src/server.js
@@ -6,7 +6,7 @@ import * as sapper from '@sapper/server'
 const { PORT, NODE_ENV } = process.env
 const dev = NODE_ENV === 'development'
 
-polka()
+export default polka()
 	.use(
 		compression({ threshold: 0 }),
 		sirv('static', { dev }),
