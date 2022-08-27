9250d0d0b5f4a489afb3acb70551e21e350fb5c7
qxb3 <qxbthree@gmail.com>
2022-08-27 19:40:02 +0800

    server: using vercel serverless

--- packages/server/src/index.js
+++ packages/server/src/index.js
@@ -3,12 +3,17 @@ require('dotenv/config')
 const { build } = require('./app')
 const { connectToDb } = require('./lib/utils/db')
 
-connectToDb(() => {
-  const app = build()
+const app = build()
 
+connectToDb(() => {
   app.listen({ port: process.env.PORT || 5000 }, (err, address) => {
     if (err) return console.log(err)
 
     console.log(`Server is running at ${address}`)
   })
 })
+
+module.exports = async (req, res) => {
+  await app.ready()
+  app.server.emit('request', req, res)
+}
