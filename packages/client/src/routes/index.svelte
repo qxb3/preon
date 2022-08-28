20fd0d1b9053ef78ddfb2d7507ef7cabe5b27a34
qxb3 <qxbthree@gmail.com>
2022-08-28 13:14:42 +0800

    change description

--- packages/client/src/routes/index.svelte
+++ packages/client/src/routes/index.svelte
@@ -50,8 +50,8 @@
 
 <div class="container">
 	<div class="text-center mt-5 mb-4">
-		<h1 style="font-size: 1.7rem">preon</h1>
-		<p style="font-size: 1.4rem;">A free url shortener</p>
+		<h1 style="font-size: 1.9rem">preon</h1>
+		<p style="font-size: 1.2rem;">A free and open source url shortener</p>
 	</div>
 
 	<form on:submit|preventDefault={shorten} novalidate>
