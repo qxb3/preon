7e05429f0bfb32c1a42f07535ef0a900290bb3a6
qxb3 <qxbthree@gmail.com>
2022-08-28 22:40:28 +0800

    server: remove app.listen() on serverless.js

--- packages/server/api/serverless.js
+++ packages/server/api/serverless.js
@@ -6,12 +6,6 @@ const { connectToDb } = require('../src/lib/utils/db')
 connectToDb(() => {
   const app = build()
 
-  app.listen({ port: process.env.PORT || 5000 }, (err, address) => {
-    if (err) return console.log(err)
-
-    console.log(`Server is running at ${address}`)
-  })
-
   module.exports = async (req, res) => {
     await app.ready()
 
