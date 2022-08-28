f44b9490c4da15ae92ef509732f92783f734910e
qxb3 <qxbthree@gmail.com>
2022-08-28 13:12:19 +0800

    client: added how to use, faq, and footer

---
+++ packages/client/src/components/Footer.svelte
@@ -0,0 +1,25 @@
+<script>
+  const githubProfile = 'https://github.com/qxb3'
+</script>
+
+<footer class="text-center mt-5">
+  <div class="d-flex flex-row justify-content-center">
+    <a href="{githubProfile}/preon" class="m-2">
+      <img src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png" alt="github" width="32px" height="32px">
+    </a>
+
+    <a href="https://qxb3.tk" class="m-2">
+      <img src="https://www.iconpacks.net/icons/2/free-icon-web-search-4289.png" alt="browser" width="32px" height="32px">
+    </a>
+  </div>
+
+  <div>
+    <p>Built by <a href={githubProfile} target="_blank">qxb3</a></p>
+  </div>
+</footer>
+
+<style>
+	a {
+		text-decoration: none;
+	}
+</style>
--- packages/client/src/routes/index.svelte
+++ packages/client/src/routes/index.svelte
@@ -3,6 +3,7 @@
 
 	import UrlModal from '../components/UrlModal.svelte'
 	import StatsModal from '../components/StatsModal.svelte'
+	import Footer from '../components/Footer.svelte'
 
 	let url = ''
 	let error = ''
@@ -10,10 +11,13 @@
 
 	let urlModal = null
 	let statsModal = null
+	let origin = ''
 
 	onMount(() => {
 		urlModal = new bootstrap.Modal(document.getElementById('urlmodal'))
 		statsModal = new bootstrap.Modal(document.getElementById('statsmodal'))
+
+		origin = location.origin
 	})
 
 	async function shorten() {
@@ -21,7 +25,7 @@
 			return error = 'Url is required'
 
 		const { API_URL } = env
-		const res = await fetch(`${API_URL}/shorten?url=${url}&hostname=${location.origin}`, { method: 'POST' })
+		const res = await fetch(`${API_URL}/shorten?url=${url}&hostname=${origin}`, { method: 'POST' })
 		const data = await res.json()
 
 		if (res.status !== 200)
@@ -41,6 +45,9 @@
 	}
 </script>
 
+<UrlModal {...shortenedUrl} />
+<StatsModal on:search={stats} />
+
 <div class="container">
 	<div class="text-center mt-5 mb-4">
 		<h1 style="font-size: 1.7rem">preon</h1>
@@ -59,6 +66,39 @@
 		<button on:click={() => statsModal.show()} class="btn btn-link">Url Stats</button>
 	</div>
 
-	<UrlModal {...shortenedUrl} />
-	<StatsModal on:search={stats} />
+	<section class="mt-5">
+		<h3>• How to use</h3>
+		<ul>
+			<li>
+				Paste the url that you want to shorten. <br>
+				(Please note that you need to include the http:// or https://) <br>
+				Then click "Shorten" Button
+			</li>
+			<li>
+				To see how many clicks your shortened url has <br>
+				Click the "Url Stats" Button then paste the id of your shortend url <br>
+				You can see this id at the end of your url <br>
+				Example: {origin}/8TW_3jxl Then Your id is 8TW_3jxl
+			</li>
+		</ul>
+	</section>
+
+	<section class="mt-4">
+		<h3>• Faq</h3>
+		<ol>
+			<li>
+				Why preon? <br>
+				- I want to name this project on the smallest thing on the world
+				and preon is what i found
+			</li>
+		</ol>
+	</section>
 </div>
+
+<Footer />
+
+<style>
+	.btn-link {
+		text-decoration: none;
+	}
+</style>
