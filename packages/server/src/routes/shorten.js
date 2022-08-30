2a7482484f16a227b7424121eb31c4facf1fbbd0
qxb3 <qxbthree@gmail.com>
2022-08-30 21:24:55 +0800

    server: fixing the shit out of it (3)

--- packages/server/src/routes/shorten.js
+++ packages/server/src/routes/shorten.js
@@ -11,27 +11,26 @@ const opts = {
 }
 
 module.exports = (fastify, _, done) => {
-  fastify.get('/shorten', opts, async (req, reply) => {
-    reply.send({ foo: 1 })
-    // const { url } = req.query
-    // const { origin } = req.headers
+  fastify.post('/shorten', opts, async (req, reply) => {
+    const { url } = req.query
+    const { origin } = req.headers
 
-    // if (!url) return reply.badRequest('query \'url\' is required')
-    // if (!validateUrl(url)) return reply.badRequest('url is not a valid link')
+    if (!url) return reply.badRequest('query \'url\' is required')
+    if (!validateUrl(url)) return reply.badRequest('url is not a valid link')
 
-    // try {
-    //   const id = crypto.randomBytes(6).toString('base64url')
-    //   const result = await urls.create({
-    //     id,
-    //     url: `${origin}/${id}`,
-    //     origin: url
-    //   })
+    try {
+      const id = crypto.randomBytes(6).toString('base64url')
+      const result = await urls.create({
+        id,
+        url: `${origin}/${id}`,
+        origin: url
+      })
 
-    //   reply.send(result)
-    // } catch(err) {
-    //   console.log(err)
-    //   reply.internalServerError()
-    // }
+      reply.send(result)
+    } catch(err) {
+      console.log(err)
+      reply.internalServerError()
+    }
   })
 
   done()
